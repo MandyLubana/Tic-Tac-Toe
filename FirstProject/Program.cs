a30532f6611548320ee69d0ad873035fc5fcaa96
maninder pal singh <s.empi@yahoo.it>
2017-06-09 15:41:02 -0400

    created project with dafault code

---
+++ FirstProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 12;
+            double garde = 89.9;
+            bool jasleen = true;
+            string talk = "I said string";
+            char iAmALetter = 's';
+
+            Console.WriteLine(age);
+            Console.ReadKey();
+            Console.WriteLine(talk);
+            Console.ReadKey();
+        }
+    }
+}
